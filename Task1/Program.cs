6d8447463e6704a18b0332956859b2d24c53ca75
Elina <elinasarkina17@gmail.com>
2024-02-19 15:11:18 +0100

    add cwfeb24:task1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿string[]originalArray = {"машина","лес","девочка","дело","три"};
+
+string[]newArray = FilterArrayByLength(originalArray,3);
+Console.WriteLine("OriginalArray:" + string.Join(",", originalArray));
+Console.WriteLine("NewArray:" + string.Join(",", newArray));
+
+static string[]FilterArrayByLength(string[]originalArray, int maxLength)
+{
+    int count = 0;
+    foreach(string str in originalArray)
+    {
+        if(str.Length <= maxLength)
+        {
+            count++;
+        }
+    }
+    string[]newArray = new string[count];
+    int index = 0;
+    foreach(string str in originalArray)
+    {
+        if(str.Length <= maxLength)
+        {
+            newArray[index] = str;
+            index++;
+        }
+    }
+    return newArray;
+}
+
+   
