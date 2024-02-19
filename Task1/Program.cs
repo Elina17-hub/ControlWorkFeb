7fd08a2a0b7be86b6324d54a56e0dd0ab335edba
Elina <elinasarkina17@gmail.com>
2024-02-19 17:56:59 +0100

    add new chart

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,28 +1,33 @@
-﻿string[]originalArray = {"машина","лес","девочка","дело","три","123"};
+﻿//СХЕМА
+//1.Создать блок для ввода исходного массива строк
+//2.Создать блок для инициализации переменных, необходимых для подсчета кол-ва строк
+// в новом массиве и создания нового массива.
+//3. Следующий блок проверяет каждую строку в исходном массиве на условие <=3 символам
+//4. если да, то увеличиваем счетчик количества строк в новом массиве
+//5. созлаем новый массив с размером, равным количеству строк, найденом в предыдущем шаге
+//6. следующий блок проверяет каждую строку в исходном массиве на условие <=3 символам
+//7. если да, добавляет строку в новый массив
+//8. блок для вывода нового массива на консоль
+
+
+string[]originalArray = {"москва","берлин","париж","-2","123","???"};
 Console.WriteLine("OriginalArray:" + string.Join(",", originalArray));
-string[]newArray = FilterArrayByLength(originalArray,3);
-Console.WriteLine("NewArray:" + string.Join(",", newArray));
-static string[]FilterArrayByLength(string[]originalArray, int maxLength)
+int newSize = 0;
+foreach(string str in originalArray)
 {
-    int count = 0;
-    foreach(string str in originalArray)
+    if(str.Length <= 3)
+    newSize++;
+}
+string[]newArray = new string[newSize];
+int currentIndex = 0;
+foreach(string str in originalArray)
+{
+    if(str.Length <= 3)
     {
-        if(str.Length <= maxLength)
-        {
-            count++;
-        }
+        newArray[currentIndex] = str;
+        currentIndex++;
     }
-    string[]newArray = new string[count];
-    int index = 0;
-    foreach(string str in originalArray)
-    {
-        if(str.Length <= maxLength)
-        {
-            newArray[index] = str;
-            index++;
-        }
-    }
-    return newArray;
 }
 
-   
+ Console.WriteLine("NewArray:" + string.Join(",", newArray));
+
